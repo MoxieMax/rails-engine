d3f67c8e52ae67b7c49900108cbc8d28f2957f76
Max MacGillivray <mary.macgillivray@gmail.com>
2023-02-08 10:35:19 -0700

    Setup is mostly complete, no tests exist but initially generated ones

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  
+end
+# Items:
+# get all items
+# get one item
+# create an item
+# edit an item
+# delete an item
+# get the merchant data for a given item ID
---
+++ app/models/merchant.rb
@@ -0,0 +1,7 @@
+class Merchant < ApplicationRecord
+  
+end
+# Merchants:
+# get all merchants
+# get one merchant
+# get all items for a given merchant ID
