da8143e0d3841d3f9564484fc59a2f13e3ff7fb4
Max MacGillivray <mary.macgillivray@gmail.com>
2023-02-10 09:59:20 -0700

    Removed folders with unused controllers

--- app/controllers/api/v1/items/search_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::Items::SearchController < ApplicationController
-  def index
-    
-  end
-end
--- app/controllers/api/v1/merchants/search_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::Merchants::SearchController < ApplicationController
-  def index
-    
-  end
-end
