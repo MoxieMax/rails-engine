5d9a3cd539279bbacaebae863da47b7f6c010a0c
Max MacGillivray <mary.macgillivray@gmail.com>
2023-03-28 23:12:00 -0700

    Created tests up to merchant error, all test but one are passing in postman

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,27 +2,60 @@ require 'rails_helper'
 
 RSpec.describe "Merchants API", type: :request do
   describe "RESTful Endpoints" do
-    before :each do
-      create_list(:merchant, 5)
+    describe "#index" do
+      before :each do
+        create_list(:merchant, 5)
+        
+        get api_v1_merchants_path
+        
+        expect(response).to be_successful 
+        
+        @merchants = JSON.parse(response.body, symbolize_names: true)
+        @merchant  = @merchants[:data].first 
+      end
       
-      get api_v1_merchants_path
+      it "returns all merchants" do
+        expect(@merchants).to be_a(Hash)
+        expect(@merchants[:data]).to be_an(Array)
+        
+        expect(@merchants[:data].count).to eq(5)
+        expect(@merchants[:data].count).to_not eq(10)
+      end
       
-      expect(response).to be_successful 
-      
-      @merchants = JSON.parse(response.body, symbolize_names: true)
-      @merchant  = @merchants[:data].first 
+      it "can access a single merchant and its attributes" do
+        expect(@merchant).to be_a(Hash)
+        expect(@merchant).to have_key(:id)
+        expect(@merchant).to have_key(:type)
+        expect(@merchant).to have_key(:attributes)
+        
+        expect(@merchant[:type]).to eq("merchant")
+        expect(@merchant[:attributes]).to have_key(:name)
+      end
     end
     
-    it "#index returns all merchants" do
-      expect(@merchants).to be_a(Hash)
-      expect(@merchants[:data]).to be_an(Array)
+    describe "#show" do
+      before :each do
+        @merchant = create(:merchant)
+
+        get api_v1_merchants_path(@merchant)
+
+        expect(response).to be_successful
+
+        @merchants = JSON.parse(response.body, symbolize_names: true)
+      end
       
-      expect(@merchants[:data].count).to eq(5)
-      expect(@merchants[:data].count).to_not eq(10)
+      it "returns a specified merchant" do
+        expect(@merchant).to be_a(Merchant)
+        expect(@merchant.id).to be_an(Integer)
+        expect(@merchant.name).to be_a(String)
+      end
       
-      # expect(@merchants[:data]).to
-      # expect(@merchants[:data]).to
-      # expect(@merchants[:data]).to
+      xit "returns a 404 error for an invalid ID" do
+        get '/api/v1/merchants/24601'
+        
+        expect(response).to_not be_successful
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
